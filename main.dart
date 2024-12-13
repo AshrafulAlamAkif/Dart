57189c8e100a4d449bad23d230902b80fba90cff
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2024-12-13 23:30:34 +0600

    First day

---
+++ main.dart
@@ -0,0 +1,20 @@
+void main(){
+  // list
+  List number = [2,3,5,8,9];
+  print(number[3]);
+
+  // map
+  Map<String,dynamic> MyMap = {
+    'name': "akif",
+    'age': 6,
+  };
+  print(MyMap);
+
+  //set
+  Set Myset = {
+    2,3,5,8,4,7
+  };
+
+print(Myset);
+
+}

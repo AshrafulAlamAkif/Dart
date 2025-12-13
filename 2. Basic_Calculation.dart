c78ce8dd92a37941ac9b3c2df8958f8999e04027
Ashraful Alam Akif <ashraful15-2225@diu.edu.bd>
2025-12-13 17:55:56 +0600

    Add basic arithmetic operations in main function

---
+++ 2. Basic_Calculation.dart
@@ -0,0 +1,15 @@
+void main(){
+  int num1 = 10;
+  int num2 = 20;
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: $sum");
+
+  int difference = num2- num1;
+  print("The difference between $num2 and $num1 is: $difference");
+
+  int multiplication = num1 * num2;
+  print("The multiplication of $num1 and $num2 is: $multiplication");
+
+  double division = num2/ num1;
+  print("The divison of $num2 by $num1 is: $division");
+}

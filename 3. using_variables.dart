1f1ecf849e59cc43e79aab507b2136734cdd6507
Ashraful Alam Akif <ashraful15-2225@diu.edu.bd>
2025-12-13 17:56:16 +0600

    Add initial implementation of variable usage in main function

---
+++ 3. using_variables.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = "Akif";
+  String address = "USA";
+  int age = 26;
+  double height = 5.9;
+  bool isMarried = false;
+
+  print("Name: $name");
+  print("Address: $address");
+  print("Age: $age");
+  print("Height: $height");
+  print("Marital Status: $isMarried");
+}

e0a80a77a4fc4a61a3e54a712f419b7d4bd069aa
AshrafulAlamAkif <ashraful15-2225@diu.edu.bd>
2026-05-21 20:52:00 +0600

    using this keyword

---
+++ Constructor/7.3 Named const.dart
@@ -0,0 +1,20 @@
+class Person{
+    String? name;
+    int? age;
+    String? address;
+    
+    Person(this.name, this.age, this.address);
+    // using this keyword in named constructor
+    Person.admin(this.name, this.age, this.address);
+}
+void main(){
+    Person p1 = Person("Akif", 27, "bangladesh");
+    print("Person p1 info is: ${p1.name}");
+    print("Person p1 age is: ${p1.age}");
+    print("Person p1 address is: ${p1.address}\n");
+    
+    Person p2 = Person.admin("Admin", 25, "Dhaka");
+    print("Person p2 info is: ${p2.name}");
+    print("Person p2 age is: ${p2.age}");
+    print("Person p2 address is: ${p2.address}");
+}
